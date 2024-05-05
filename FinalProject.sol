f8d2ce66bb7230fcc6fa92f3f5117572deeb7a83
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-05-05 19:26:42 +0600

    Transaction history added

--- FinalProject.sol
+++ FinalProject.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.21;
 contract CashLess {
 
     address  authority;
-    uint8 authorityMobile;
+    uint authorityMobile;
     
     struct Person {
         string name;
-        uint8 mobile;
+        uint mobile;
         string password;
         uint balance;
         address walletAddress;
@@ -16,19 +16,27 @@ contract CashLess {
     }
 
     struct Mobile{
-        uint8 mobile;
+        uint mobile;
     }
 
-    mapping (uint8 => Person)  users;
-    mapping  (address => Mobile) userMobile;
+    struct Transaction {
+        uint from;
+        uint to;
+        uint amount;
+        uint timestamp;
+    }
 
-    constructor (string memory _name, uint8 _mobile, string memory _password) {
+    mapping (uint => Person)  users;
+    mapping  (address => Mobile) userMobile;
+    mapping (address => Transaction[]) transactionHistory;
+
+    constructor (string memory _name, uint _mobile, string memory _password) {
         authority = msg.sender;
         authorityMobile = _mobile;
         createAccount(_name, _mobile, _password);
     }
 
-    function createAccount (string memory _name, uint8 _mobile,  string memory _password) public {
+    function createAccount (string memory _name, uint _mobile,  string memory _password) public {
 
         require(users[_mobile].created == false,"You already have an account.");
         users[_mobile] = Person({
@@ -46,44 +54,58 @@ contract CashLess {
     }
 
     
-    function addMoney (uint _amount, uint8 _mobile) public {
+    function addMoney (uint _amount) public {
         require(msg.sender == authority, "You cannot add tokens to authority account");
-        users[_mobile].balance += _amount;
+        users[authorityMobile].balance += _amount;
+        addToTransactionHistory(authorityMobile, authorityMobile, _amount);
     }
 
-    function transfer (uint8 _from, uint8 _to, uint _amount) internal {
+    function transfer (uint _from, uint _to, uint _amount) internal {
         users[_from].balance -= _amount;
         users[_to].balance += _amount;
+        addToTransactionHistory(_from, _to, _amount);
     }
 
-    function setBalance (uint8 _mobile, uint _amount) public {
+    function addToTransactionHistory(uint _from, uint _to, uint _amount) private {
+        transactionHistory[users[_from].walletAddress].push(Transaction({
+            from: _from,
+            to: _to,
+            amount: _amount,
+            timestamp: block.timestamp
+        }));
+    }
+    function getTransactionHistory() public view returns (Transaction[] memory) {
+        return transactionHistory[msg.sender];
+    }
+
+    function setBalance (uint _mobile, uint _amount) public {
         require(msg.sender==authority, "Only Authority can give tokens");
-        require(users[_mobile].balance >= _amount, "You don't have sufficient balance. Pelease add tokens.");
+        require(users[authorityMobile].balance >= _amount, "You don't have sufficient balance. Pelease add tokens.");
         require(users[_mobile].created == true, "Account not created yet.");
         transfer(authorityMobile, _mobile, _amount);
     }
 
-    function makePayment (uint8 _seller, uint _amount) external {
-        uint8 _mobile = userMobile[msg.sender].mobile;
+    function makePayment (uint _seller, uint _amount) external {
+        uint _mobile = userMobile[msg.sender].mobile;
         require(users[_mobile].balance >= _amount, "You don't have sufficient balance to payment. Please add tokens.");
         require(users[_seller].created == true, "Account not created yet.");
         transfer(_mobile, _seller, _amount);
     }
 
     function withDraw (uint _amount) external {
-        uint8 _mobile = userMobile[msg.sender].mobile;
+        uint _mobile = userMobile[msg.sender].mobile;
         require(users[_mobile].balance >= _amount, "You don't have sufficient balance to withdraw.");
         require(msg.sender != authority, "Authority can't withdraw.");
         transfer(_mobile, authorityMobile, _amount);
     }
 
     function getBalance () public view returns (uint) {
-        uint8 _mobile = userMobile[msg.sender].mobile;
+        uint _mobile = userMobile[msg.sender].mobile;
         return users[_mobile].balance;
     }
 
-    function Details () public view returns (string memory, uint8, address) {
-        uint8 _mobile = userMobile[msg.sender].mobile;
+    function Details () public view returns (string memory, uint, address) {
+        uint _mobile = userMobile[msg.sender].mobile;
         return (
             users[_mobile].name,
             users[_mobile].mobile,
@@ -91,7 +113,7 @@ contract CashLess {
 
         );
     }
-    function authorityDetails () public view returns (string memory, uint8, address) {
+    function authorityDetails () public view returns (string memory, uint, address) {
          return (
             users[authorityMobile].name,
             users[authorityMobile].mobile,
@@ -100,7 +122,7 @@ contract CashLess {
     }
 
     function loginCheck (string memory _password) external view returns (string memory) {
-        uint8 _mobile = userMobile[msg.sender].mobile;
+        uint _mobile = userMobile[msg.sender].mobile;
         if ( (_mobile == authorityMobile) &&
             keccak256(bytes(users[authorityMobile].password)) == keccak256(bytes(_password))
         ) {
