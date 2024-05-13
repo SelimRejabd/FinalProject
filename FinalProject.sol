12b2c827ec54bc2c96ff06e49978c9cdbcb4f573
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-05-13 21:16:41 +0600

    phone number data type changed to string

--- FinalProject.sol
+++ FinalProject.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.21;
 contract CashLess {
 
     address  authority;
-    uint authorityMobile;
+    string authorityMobile;
     
     struct Person {
         string name;
-        uint mobile;
+        string mobile;
         string password;
         uint balance;
         address walletAddress;
@@ -16,29 +16,45 @@ contract CashLess {
     }
 
     struct Mobile{
-        uint mobile;
+        string mobile;
     }
 
     struct Transaction {
-        uint from;
-        uint to;
+        string from;
+        string to;
         uint amount;
         uint timestamp;
     }
 
-    mapping (uint => Person)  users;
+    mapping (string => Person)  users;
     mapping  (address => Mobile) userMobile;
     mapping (address => Transaction[]) transactionHistory;
 
-    constructor (string memory _name, uint _mobile, string memory _password) {
+    constructor (string memory _name, string memory _mobile, string memory _password) {
         authority = msg.sender;
         authorityMobile = _mobile;
         createAccount(_name, _mobile, _password);
     }
 
-    function createAccount (string memory _name, uint _mobile,  string memory _password) public {
+    function createAccount (string memory _name, string memory _mobile,  string memory _password) public {
 
         require(users[_mobile].created == false,"You already have an account.");
+        users[_mobile] = Person({
+            name: _name,
+            mobile : _mobile,
+            password: _password,
+            balance: users[_mobile].balance,
+            walletAddress: msg.sender,
+            created : true
+        });
+
+        userMobile[msg.sender] = Mobile({
+            mobile: _mobile
+        });
+    }
+
+    function updateInfo (string memory _name, string memory _mobile,  string memory _password) public {
+        require(users[_mobile].created == true,"You have n't any account. Please register.");
         users[_mobile] = Person({
             name: _name,
             mobile : _mobile,
@@ -60,13 +76,13 @@ contract CashLess {
         addToTransactionHistory(authorityMobile, authorityMobile, _amount);
     }
 
-    function transfer (uint _from, uint _to, uint _amount) internal {
+    function transfer (string memory _from, string memory _to, uint _amount) internal {
         users[_from].balance -= _amount;
         users[_to].balance += _amount;
         addToTransactionHistory(_from, _to, _amount);
     }
 
-    function addToTransactionHistory(uint _from, uint _to, uint _amount) private {
+    function addToTransactionHistory(string memory _from, string memory _to, uint _amount) private {
         transactionHistory[users[_from].walletAddress].push(Transaction({
             from: _from,
             to: _to,
@@ -84,34 +100,34 @@ contract CashLess {
         return transactionHistory[msg.sender];
     }
 
-    function setBalance (uint _mobile, uint _amount) public {
+    function setBalance (string memory _mobile, uint _amount) public {
         require(msg.sender==authority, "Only Authority can give tokens");
         require(users[authorityMobile].balance >= _amount, "You don't have sufficient balance. Pelease add tokens.");
         require(users[_mobile].created == true, "Account not created yet.");
         transfer(authorityMobile, _mobile, _amount);
     }
 
-    function makePayment (uint _seller, uint _amount) external {
-        uint _mobile = userMobile[msg.sender].mobile;
+    function makePayment (string memory _seller, uint _amount) external {
+        string memory _mobile = userMobile[msg.sender].mobile;
         require(users[_mobile].balance >= _amount, "You don't have sufficient balance to payment. Please add tokens.");
         require(users[_seller].created == true, "Account not created yet.");
         transfer(_mobile, _seller, _amount);
     }
 
     function withDraw (uint _amount) external {
-        uint _mobile = userMobile[msg.sender].mobile;
+        string memory _mobile = userMobile[msg.sender].mobile;
         require(users[_mobile].balance >= _amount, "You don't have sufficient balance to withdraw.");
         require(msg.sender != authority, "Authority can't withdraw.");
         transfer(_mobile, authorityMobile, _amount);
     }
 
     function getBalance () public view returns (uint) {
-        uint _mobile = userMobile[msg.sender].mobile;
+        string memory _mobile = userMobile[msg.sender].mobile;
         return users[_mobile].balance;
     }
 
-    function Details () public view returns (string memory, uint, address) {
-        uint _mobile = userMobile[msg.sender].mobile;
+    function Details () public view returns (string memory, string memory, address) {
+        string memory _mobile = userMobile[msg.sender].mobile;
         return (
             users[_mobile].name,
             users[_mobile].mobile,
@@ -119,7 +135,7 @@ contract CashLess {
 
         );
     }
-    function authorityDetails () public view returns (string memory, uint, address) {
+    function authorityDetails () public view returns (string memory, string memory, address) {
          return (
             users[authorityMobile].name,
             users[authorityMobile].mobile,
@@ -128,17 +144,22 @@ contract CashLess {
     }
 
     function loginCheck (string memory _password) external view returns (string memory) {
-        uint _mobile = userMobile[msg.sender].mobile;
-        if ( (_mobile == authorityMobile) &&
-            keccak256(bytes(users[authorityMobile].password)) == keccak256(bytes(_password))
-        ) {
-            return "athority";
+    string memory _mobile = userMobile[msg.sender].mobile;
+    string memory _authorityMobile = authorityMobile;
+
+    if (keccak256(bytes(_mobile)) == keccak256(bytes(_authorityMobile))) {
+        if (keccak256(bytes(users[_authorityMobile].password)) == keccak256(bytes(_password))) {
+            return "authority";
+        } else {
+            return "notUser";
         }
-        if (
-            keccak256(bytes(users[_mobile].password)) == keccak256(bytes(_password)))
-        {
+    } else {
+        if (keccak256(bytes(users[_mobile].password)) == keccak256(bytes(_password))) {
             return "user";
+        } else {
+            return "notUser";
         }
-        return "notUser";
     }
+}
+
 }
